6c481bbc10e6496f4915d596026827e093287163
MiyueSC <913784771@qq.com>
2021-02-26 17:16:20 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,59 +2,37 @@
   <h1>{{ msg }}</h1>
 
   <button @click="() => (count ++)">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 
-    <p>{{ message }}</p>
+<p>{{ message }}</p>
     <button @click="reverseMessage">反转 Message</button>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import {toRefs, ref} from "vue";
 
-export default defineComponent({
+export default {
   name: "HelloWord",
   props: {
     msg: String
   },
-  data() {
-    return {
-      count: 0,
-      message: "Hello World!"
+  setup(props) {
+    const { msg } = toRefs(props);
+    const count = ref(0);
+    const message = ref("Hello World!")
+
+    const reverseMessage = () => {
+      message.value = message.value.split("").reverse().join("")
     }
-  },
-  methods: {
-    reverseMessage() {
-      console.log(this)
-      this.message = this.message.split("").reverse().join("")
+
+    return {
+      msg,
+      count,
+      message,
+      reverseMessage
     }
   }
-})
+}
 </script>
-
-<!--<script setup>-->
-<!--import { defineProps, reactive, ref } from 'vue'-->
-
-<!--defineProps({-->
-<!--  msg: String-->
-<!--})-->
-
-<!--const state = reactive({ count: 0 })-->
-<!--console.log(state);-->
-
-<!--const message = ref(0);-->
-<!--message.value = "hello world!"-->
-
-<!--const reverseMessage = () => {-->
-<!--  message.value = message.value-->
-<!--      .split('')-->
-<!--      .reverse()-->
-<!--      .join('')-->
-<!--}-->
-<!--console.log(message);-->
-<!--</script>-->
 
 <style scoped>
 a {
